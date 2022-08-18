58b7a2b8be4305a5d13d3a1a847aca6d6352f93a
omar201509 <omar20150923@gmail.com>
2022-08-18 18:52:37 +0300

    Condition If-else

---
+++ Ex004/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "omar")
+{
+    Console.WriteLine ("Ура,Это же OMAR!");
+}
+else
+{
+    Console.WriteLine ("Привет,");
+    Console.WriteLine (username);
+}
